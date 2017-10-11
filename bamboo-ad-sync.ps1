10552389da98e27b6459ce25972a0fc94aa59c5e
Phil Thomson <thomson.p@gmail.com>
2017-10-11 14:32:29 -0700

    updates to script

--- bamboo-ad-sync.ps1
+++ bamboo-ad-sync.ps1
@@ -1,4 +1,4 @@
-﻿aram([switch]$testrun,[switch]$checkusers,[switch]$run)
+﻿param([switch]$testrun,[switch]$checkusers,[switch]$run)
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 #---------------------------------------------------------- 
 # LOAD MODULES 
@@ -17,16 +17,19 @@ Catch
 #---------------------------------------------------------- 
 $date = Get-Date
 $name = Import-Csv ".\name-map.csv"
-$ou="OU=users,OU=test,DC=test,DC=local"
-$pass = "admin123!"
-$log = ".\log.txt"
+$ou="CN=Users,DC=Procera,DC=lan"
+$pass = ""
+$log = ".\logs\$(Get-Date -Format 'MMM-dd-yyyy_hh-mm-ss')-log.txt"
 $setpass = ConvertTo-SecureString -AsPlainText $pass -force 
+$daysback = "-365"
+$curdate = get-date
+$datetodel = $curDate.AddDays($Daysback)
 #---------------------------------------------------------- 
 #LOAD IN USERS FROM BAMBOO HR
 #----------------------------------------------------------
 $headers = @{}
 #API string added into header variable
-$headers.Add("Authorization","Basic ")
+$headers.Add("Authorization","Basic ###")
 #Need to specify json in the header to get back psobject variable 
 $headers.Add("Accept","application/json")
 $apiurl = "https://proceranetworks.bamboohr.com/api/gateway.php/proceranetworks/v1/employees/directory"
@@ -35,6 +38,8 @@ $result = Invoke-RestMethod -Uri $apiurl -Headers $headers
 #---------------------------------------------------------- 
 #MAIN SCRIPT 
 #---------------------------------------------------------- 
+gci .\logs -Filter "*log.txt" | Where-Object { $_.lastwritetime -lt $datetodel} | Remove-Item
+
 if ($testrun)
 {
     "**Performing test run no users will be created or updated - " + $date + "**"| Out-File $log -Append
@@ -54,15 +59,22 @@ if ($testrun)
         $managertemp = $manager.supervisor.Split('-')[-1].tolower()
         $managersam = $manager.supervisor.Substring(0,1).tolower() + $managertemp.split()[-1]
 
+        foreach($y in $name)
+        {
+            if($y.bamboo -eq $($manager.'supervisor'))
+            {
+                $managersam = $y.ad
+            }
+        }
         $displayname = $($x.displayname).TrimEnd()
 
         #Check if user exists in AD
-        $exists = Get-ADUser -server win2012.test.local -Credential $cred -Filter "displayname -like '$displayname'" -Properties officephone
+        $exists = Get-ADUser -Filter "displayname -like '$displayname'" -Properties officephone
         
         if($exists)
         {
             "[TESTRUN-UPDATE] Found $($exists.'DistinguishedName') the script will update these details in non-test run:" | Out-File $log -Append
-            "`t`t GivenName: $($x.'firstName') `r`n `t`t Surname: $($x.'lastName') `r`n `t`t Department: $($x.'department') `r`n `t`t Title: $($x."jobTitle") `r`n `t`t Description: $($x."jobTitle") `r`n `t`t Display Name: $($x.'displayName') `r`n `t`t Office: $($x.'location') `r`n `t`t Manager: $($manager.'supervisor')" | Out-File $log -Append
+            "`t`t Department: $($x.'department') `r`n `t`t Title: $($x."jobTitle") `r`n `t`t Description: $($x."jobTitle") `r`n `t`t Office: $($x.'location') `r`n `t`t Manager: $($manager.'supervisor') - $managersam" | Out-File $log -Append
             
             if($($x.'workPhone') -ne $null)
             {
@@ -80,7 +92,7 @@ if ($testrun)
         }
         if(!$exists)
         {
-            $exists2 = Get-ADUser -server win2012.test.local -Credential $cred -Filter "samAccountName -eq '$sam'" -Properties officephone
+            $exists2 = Get-ADUser -Filter "samAccountName -eq '$sam'" -Properties officephone
             if (!$exists2)
             {
                 $namemap="off"
@@ -95,22 +107,24 @@ if ($testrun)
                 if($namemap -eq "off")
                 {
                     "[TESTRUN-CREATE] Could not find $($x.displayName) OR $sam in Active Directory, the script will create user in non-test run:" | Out-File $log -Append
+                    "`t`t Department: $($x.'department') `r`n `t`t Displayname: $($x.'displayName') `r`n `t`t GivenName: $($x.'firstName') `r`n `t`t Surname: $($x.'lastName') `r`n `t`t SamAccountName: $sam `r`n `t`t Office Phone: $($x.'workPhone') `r`n `t`t Title: $($x."jobTitle") `r`n `t`t Description: $($x."jobTitle") `r`n `t`t Office: $($x.'location') `r`n `t`t Manager: $($manager.'supervisor') - $managersam" | Out-File $log -Append
+                    "[TESTRUN-CREATE] Finished creating $($x.'DisplayName') in Active Directory" | Out-File $log -Append
                 }
             }
             elseif ($exists2)
             {
                 "[TESTRUN-UPDATE] Found $($exists2.'DistinguishedName') the script will update these details in non-test run:" | Out-File $log -Append
-                "`t`t GivenName: $($x.'firstName') `r`n `t`t Surname: $($x.'lastName') `r`n `t`t Department: $($x.'department') `r`n `t`t Title: $($x."jobTitle") `r`n `t`t Description: $($x."jobTitle") `r`n `t`t Display Name: $($x.'displayName') `r`n `t`t Office: $($x.'location') `r`n `t`t Email Address: $($x.'workEmail') `r`n `t`t Manager: $($manager.'supervisor')" | Out-File $log -Append
+                "`t`t Department: $($x.'department') `r`n `t`t Title: $($x."jobTitle") `r`n `t`t Description: $($x."jobTitle") `r`n `t`t Office: $($x.'location') `r`n `t`t Manager: $($manager.'supervisor') - $managersam" | Out-File $log -Append
                 
                 if($($x.'workPhone') -ne $null)
                 {
-                    if($exists.OfficePhone -eq $null)
+                    if($exists2.OfficePhone -eq $null)
                     {
                         "`t`t Office Phone: $($x.'workPhone')" | Out-File $log -Append
                     }
-                    elseif($exists.OfficePhone -ne $null)
+                    elseif($exists2.OfficePhone -ne $null)
                     {
-                        "`t`t ##Office Phone already set to: $($exists.OfficePhone) skipping update" | Out-File $log -Append
+                        "`t`t ##Office Phone already set to: $($exists2.OfficePhone) skipping update" | Out-File $log -Append
                     }
                 }   
                     "[TESTRUN-UPDATE] finshed updating user $($x.'DisplayName') in Active Directory" | Out-File $log -append
@@ -129,7 +143,7 @@ if ($checkusers)
         $displayname = $($x.displayname).TrimEnd()
 
         #Check if user exists in AD
-        $exists = Get-ADUser -server win2012.test.local -Credential $cred -Filter "displayname -like '$displayname'"
+        $exists = Get-ADUser -Filter "displayname -like '$displayname'"
 
         foreach($y in $name)
         {
@@ -140,7 +154,7 @@ if ($checkusers)
         }  
         if(!$exists)
         {
-            $exists2 = Get-ADUser -server win2012.test.local -Credential $cred -Filter "samAccountName -eq '$sam'"
+            $exists2 = Get-ADUser -Filter "samAccountName -eq '$sam'"
             
             if (!$exists2)
             {
@@ -167,25 +181,37 @@ if ($run)
         $managertemp = $manager.supervisor.Split('-')[-1].tolower()
         $managersam = $manager.supervisor.Substring(0,1).tolower() + $managertemp.split()[-1]
 
+        foreach($y in $name)
+        {
+            if($y.bamboo -eq $($manager.'supervisor'))
+            {
+                $managersam = $y.ad
+            }
+        }
+
         $displayname = $($x.displayname).TrimEnd()
 
         #Check if user exists in AD
-        $exists = Get-ADUser -server win2012.test.local -Credential $cred -filter "displayname -like '$displayname'" -Properties officephone
+        $exists = Get-ADUser -filter "displayname -like '$displayname'" -Properties officephone
 
         if($exists)
         {
             "[INFO-UPDATE]`t Found $($exists.'DistinguishedName') updating  to:" | Out-File $log -Append
-            "`t`t GivenName: $($x.'firstName') `r`n `t`t Surname: $($x.'lastName') `r`n `t`t Department: $($x.'department') `r`n `t`t Title: $($x."jobTitle") `r`n `t`t Description: $($x."jobTitle") `r`n `t`t Display Name: $($x.'displayName') `r`n `t`t Office: $($x.'location') `r`n `t`t Manager: $($manager.'supervisor')" | Out-File $log -Append
-            set-aduser -server win2012.test.local -Credential $cred -Identity $sam -GivenName $($x.'firstName') -Surname $($x.'lastName') -Department $($x.'department') -Title $($x."jobTitle") -Description $($x."jobTitle")  -Office $($x.'location') -DisplayName $($x.'displayName')
-             #User details will not update if it can't find the manager so it's on seperate action
-            set-aduser -server win2012.test.local -Credential $cred -Identity $sam -Manager "$managersam" -ErrorAction SilentlyContinue
+            "`t`t Department: $($x.'department') `r`n `t`t Title: $($x."jobTitle") `r`n `t`t Description: $($x."jobTitle") `r`n `t`t Office: $($x.'location') `r`n `t`t Manager: $($manager.'supervisor')" | Out-File $log -Append
+            $exists | set-aduser -Department $($x.'department') -Title $($x."jobTitle") -Description $($x."jobTitle")  -Office $($x.'location')
+            #set-aduser -Identity $sam -Department $($x.'department') -Title $($x."jobTitle") -Description $($x."jobTitle")  -Office $($x.'location') 
+            
+            #User details will not update if it can't find the manager so it's on seperate action
+            $exists | set-aduser -Manager "$managersam" -ErrorAction SilentlyContinue
+            #set-aduser  -Identity $sam -Manager "$managersam" -ErrorAction SilentlyContinue
 
             if($($x.'workPhone') -ne $null)
             {
                 if($exists.OfficePhone -eq $null)
                 {
                     "`t`t Office Phone: $($x.'workPhone')" | Out-File $log -Append
-                    set-aduser -server win2012.test.local -Credential $cred -Identity $sam -OfficePhone $($x.'workPhone') 
+                    $exists | set-aduser -OfficePhone $($x.'workPhone')
+                    #set-aduser  -Identity $sam -OfficePhone $($x.'workPhone') 
                 }
                 elseif($exists.OfficePhone -ne $null)
                 {
@@ -197,7 +223,7 @@ if ($run)
         }
         if(!$exists)
         {
-            $exists2 = Get-ADUser -server win2012.test.local -Credential $cred -Filter "samAccountName -eq '$sam'" -Properties officephone
+            $exists2 = Get-ADUser -Filter "samAccountName -eq '$sam'" -Properties officephone
             if (!$exists2)
             {
                 $namemap="off"
@@ -211,27 +237,28 @@ if ($run)
                 }
                 if($namemap -eq "off")
                 {
-                    "[INFO-CREATE]`t user $($x.'DisplayName') does not exist in AD creating user" | Out-File $log -append 
-                    new-aduser -server win2012.test.local -Credential $cred -name $($x.'displayName') -SamAccountName $sam -GivenName $($x.'firstName') -Surname $($x.'lastName') -Department $($x.'department') -Title $($x."jobTitle") -Description $($x."jobTitle")  -Office $($x.'location') -EmailAddress $($x.'workEmail') -DisplayName $($x.'displayName') -OfficePhone $($x.'workPhone') -AccountPassword $setpass -Enabled $true -Path $ou
+                    "[INFO-CREATE]`t user $($x.'DisplayName') does not exist in AD creating user" | Out-File $log -append
+                    "`t`t Department: $($x.'department') `r`n `t`t Displayname: $($x.'displayName') `r`n `t`t GivenName: $($x.'firstName') `r`n `t`t Surname: $($x.'lastName') `r`n `t`t SamAccountName: $sam `r`n `t`t Office Phone: $($x.'workPhone') `r`n `t`t Title: $($x."jobTitle") `r`n `t`t Description: $($x."jobTitle") `r`n `t`t Office: $($x.'location') `r`n `t`t Manager: $($manager.'supervisor') - $managersam `r`n `t`t Path: $ou" | Out-File $log -Append 
+                    new-aduser  -name $($x.'displayName') -SamAccountName $sam -GivenName $($x.'firstName') -Surname $($x.'lastName') -Department $($x.'department') -Title $($x."jobTitle") -Description $($x."jobTitle")  -Office $($x.'location') -DisplayName $($x.'displayName') -OfficePhone $($x.'workPhone') -AccountPassword $setpass -Enabled $true -Path $ou
                     #User details will not update if it can't find the manager so it's on seperate action
-                    set-aduser -Server win2012.test.local -Credential $cred -Identity $sam -Manager "$managersam" -ErrorAction SilentlyContinue
+                    set-aduser  -Identity $sam -Manager "$managersam" -ErrorAction SilentlyContinue
                     "[INFO-CREATE]`t finshed creating user $sam in AD"| Out-File $log -append
                 }
             }
             elseif ($exists2)
             {
-                "[INFO-UPDATE]`t Found $($exists.'DistinguishedName') updating  to:" | Out-File $log -Append
-                "`t`t GivenName: $($x.'firstName') `r`n `t`t Surname: $($x.'lastName') `r`n `t`t Department: $($x.'department') `r`n `t`t Title: $($x."jobTitle") `r`n `t`t Description: $($x."jobTitle") `r`n `t`t Display Name: $($x.'displayName') `r`n `t`t Office: $($x.'location') `r`n `t`t Manager: $($manager.'supervisor')" | Out-File $log -Append
-                set-aduser -server win2012.test.local -Credential $cred -Identity $sam -GivenName $($x.'firstName') -Surname $($x.'lastName') -Department $($x.'department') -Title $($x."jobTitle") -Description $($x."jobTitle")  -Office $($x.'location') -DisplayName $($x.'displayName')
+                "[INFO-UPDATE]`t Found $($exists2.'DistinguishedName') updating  to:" | Out-File $log -Append
+                "`t`t Department: $($x.'department') `r`n `t`t Title: $($x."jobTitle") `r`n `t`t Description: $($x."jobTitle") `r`n `t`t Office: $($x.'location') `r`n `t`t Manager: $($manager.'supervisor')" | Out-File $log -Append
+                set-aduser  -Identity $sam -Department $($x.'department') -Title $($x."jobTitle") -Description $($x."jobTitle")  -Office $($x.'location')
                 #User details will not update if it can't find the manager so it's on seperate action
-                set-aduser -server win2012.test.local -Credential $cred -Identity $sam -Manager "$managersam" -ErrorAction SilentlyContinue
+                set-aduser  -Identity $sam -Manager "$managersam" -ErrorAction SilentlyContinue
 
                 if($($x.'workPhone') -ne $null)
                 {
                     if($exists.OfficePhone -eq $null)
                     {
                         "`t`t Office Phone: $($x.'workPhone')" | Out-File $log -Append
-                        set-aduser -server win2012.test.local -Credential $cred -Identity $sam -OfficePhone $($x.'workPhone') 
+                        set-aduser  -Identity $sam -OfficePhone $($x.'workPhone') 
                     }
                     elseif($exists.OfficePhone -ne $null)
                     {
